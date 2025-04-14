c471455fa42b939c91334649360e8b92cc0c0dd0
Gaga Demetrashvili <gagademetrashvili@credo.ge>
2025-04-15 01:16:52 +0400

    Using Fluent API for DB schema configuration

--- CompanyEmployees.Infrastructure.Persistence/Configurations/CompanyConfiguration.cs
+++ CompanyEmployees.Infrastructure.Persistence/Configurations/CompanyConfiguration.cs
@@ -4,10 +4,48 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CompanyEmployees.Infrastructure.Persistence.Configurations;
 
+// If you want to have cleaner models, without those Required, MaxLength,  and ForeignKey attributes
+// you can extract the configuration in separate files using the Fluent API approach.
+
+// Here we have initial configuration for creating our tables and columns with relationships.
+// IsRequired() method makes the property required (and the column as not null in the database).
+
+// So, this is how you can extract the configuration logic from the model classes. First,
+// you would create the initial configuration for both entities, create migrations, and update the database.
+// Then, you could add the seeding logic in both classes, create additional migrations, and update the database again.
+// You would get the same result as you have now.
+// Of course, if we decided to use this approach from the start, we wouldn’t need all those attributes inside the model classes, just clean properties.
 public class CompanyConfiguration : IEntityTypeConfiguration<Company>
 {
     public void Configure(EntityTypeBuilder<Company> builder)
     {
+        builder.ToTable("Companies")
+            .HasKey(c => c.Id);
+
+        // builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Id)
+            .HasColumnName("CompanyId")
+            .IsRequired();
+
+        builder.Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(60);
+
+        builder.Property(c => c.Address)
+            .IsRequired()
+            .HasMaxLength(60);
+
+        builder.Property(c => c.Country);
+
+        builder.HasMany(c => c.Employees)
+            .WithOne(e => e.Company)
+            .HasForeignKey(e => e.CompanyId)
+            // Configures a cascade delete behavior in Entity Framework Core.
+            // This means that if a Company entity is deleted, all related Employee entities
+            // (those with a CompanyId foreign key pointing to the deleted Company) will also be automatically deleted from the database.
+            .OnDelete(DeleteBehavior.Cascade);
+
         builder.HasData
         (
             new Company
--- CompanyEmployees.Infrastructure.Persistence/Configurations/EmployeeConfiguration.cs
+++ CompanyEmployees.Infrastructure.Persistence/Configurations/EmployeeConfiguration.cs
@@ -8,6 +8,29 @@ public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
 {
     public void Configure(EntityTypeBuilder<Employee> builder)
     {
+        builder.ToTable("Employees")
+            .HasKey(e => e.Id);
+
+        builder.Property(e => e.Id)
+            .HasColumnName("EmployeeId")
+            .IsRequired();
+
+        builder.Property(e => e.Name)
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.Property(e => e.Age)
+            .IsRequired();
+
+        builder.Property(e => e.Position)
+            .IsRequired()
+            .HasMaxLength(20);
+
+        builder.HasOne(e => e.Company)
+            .WithMany(c => c.Employees)
+            .HasForeignKey(e => e.CompanyId)
+            .OnDelete(DeleteBehavior.Cascade);
+
         builder.HasData
         (
             new Employee
